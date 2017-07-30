9cc110f08d77efe64a760e734d7cc64746b8ae9c
sarwat zareen <sz299@afsconnect2.njit.edu>
2017-07-30 11:03:06 -0400

    added index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+	// Including the database connection file
+	require_once('config.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Todo List Project</title>
+</head>
+<body>
+	<a href="todo_list_add.php"><img class="add-icon" src="images/add.png" /></a>
+	
+</body>
+</html>
