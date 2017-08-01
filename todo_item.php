1b7b678cc23429460fd79b5d3f0fad0224826ee0
sarwat zareen <sz299@afsconnect2.njit.edu>
2017-07-31 23:04:36 -0400

    added Add Todo button

--- todo_item.php
+++ todo_item.php
@@ -22,6 +22,32 @@
 </head>
 <body>
 	<h1><?=$row['name'] ?></h1>
+ 	<a href="todo_task_add.php?id=<?=$item_id?>"><img class="add-icon" src="images/add.png" /></a>
+	<table border="1">
+		<tr>
+			<th>Task Name</th>
+			<th>Edit</th>
+			<th>Delete</th>
+			<td>Complete</td>
+		</tr>
+		<?php 
+			//Get the list of tasks related to this todolist
+			//Select everything from todo_tasks table where 
+			$result = $db->query("SELECT * from `todo_tasks` WHERE `todolist_id`=".$item_id);
+			//Use foreach loop to go through the result of select
+			foreach ($result as $row) {
+		?>
+			<tr>
+				<td><?= $row['name'] ?></td>
+				<td><a href="todo_task_edit.php"><img class="add-icon" src="images/edit.png" /></a</td>
+				<td><a href="todo_task_delete.php"><img class="add-icon" src="images/delete.png" /></a></td>
+				<td></td>
+			</tr>
+		<?php 
+			}
+		?>
+	</table>
+	<br />
 	<a href="index.php">Return to Todolist</a>
 </body>
 </html>
---
+++ todo_task_add.php
@@ -0,0 +1,34 @@
+<?php 
+	// Including the database connection file
+	require_once('config.php');
+  
+    //Check to see if ID exist in GET otherwise it will show error and exit
+	if(empty($_GET['id'])){
+		echo 'Error!';
+		exit();
+	} 
+	//Get the id and store it in item_id variable
+	$item_id = $_GET['id'];
+
+    //Checking if the form has been submitted
+	if (!empty($_POST['task_name'])) {
+    // get the task name from POST
+		$task_name = $_POST['task_name'];
+    //Insert the task name into the database
+		$db->query("INSERT INTO `todo_tasks` (`name`,`todolist_id`,`is_complete`) VALUES ('$task_name','$item_id','0')");
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Todo List Project</title>
+</head>
+<body>
+	<h1>Add Task</h1>
+	<form method="POST">
+		<label>Task name:</label>
+		<input type="text" name="task_name" />
+		<input type="submit" name="submit" value="Add">
+	</form>
+</body>
+</html>
