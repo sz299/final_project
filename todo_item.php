3e7b9278fb0b72186e492dcb01bf40dc35c21a66
sarwat zareen <sz299@afsconnect2.njit.edu>
2017-08-01 20:51:30 -0400

    changing the status of todo task to complete/incomplete

--- todo_item.php
+++ todo_item.php
@@ -7,10 +7,19 @@
 		echo 'Error!';
 		exit();
 	} 
-	//Get the id and store it in item_id variable
-	$item_id = $_GET['id'];
+	//Get the id and store it in todolist_id variable
+	$todolist_id = $_GET['id'];
+
+	//Get the is_complete from GET and change the status of todo task
+	//First check if there is any value in is_complete
+	if(!empty($_GET['is_complete'])){
+		$is_complete = $_GET['is_complete'];
+		$task_id =$_GET['task_id'];
+		$db->query("UPDATE `todo_tasks` SET `is_complete`=".$is_complete." WHERE `id`=".$task_id);
+	}
+ 
 	//Do a select from the table only the row with my spesific ID
-	$result = $db->query("SELECT * FROM `todo_list` WHERE `id`=".$item_id);
+	$result = $db->query("SELECT * FROM `todo_list` WHERE `id`=".$todolist_id);
 	//Get the row and store it in $row variable
 	$row = $result->fetch();
 
@@ -22,7 +31,7 @@
 </head>
 <body>
 	<h1><?=$row['name'] ?></h1>
- 	<a href="todo_task_add.php?id=<?=$item_id?>"><img class="add-icon" src="images/add.png" /></a>
+ 	<a href="todo_task_add.php?id=<?=$todolist_id?>"><img class="add-icon" src="images/add.png" /></a>
 	<table border="1">
 		<tr>
 			<th>Task Name</th>
@@ -33,7 +42,7 @@
 		<?php 
 			//Get the list of tasks related to this todolist
 			//Select everything from todo_tasks table where 
-			$result = $db->query("SELECT * from `todo_tasks` WHERE `todolist_id`=".$item_id);
+			$result = $db->query("SELECT * from `todo_tasks` WHERE `todolist_id`=".$todolist_id);
 			//Use foreach loop to go through the result of select
 			foreach ($result as $row) {
 		?>
@@ -46,12 +55,12 @@
 						//if is_complete is 0 that means the task is not complete
 						if ($row['is_complete'] == 0) {
 					?>
-						<img src="images/checkbox.png" />
+		        <a href="todo_item.php?id=<?=$todolist_id ?>&task_id=<?=$row['id'] ?>&is_complete=true"><img src="images/checkbox.png" /></a>
 					<?php
 						} else {
 							// else is_complete is 1 which means the task is complete
 					?>
-						<img src="images/checkbox-checked.png" />
+						<a href="todo_item.php?id=<?=$todolist_id ?>&task_id=<?=$row['id'] ?>&is_complete=false"><img src="images/checkbox-checked.png" /></a>
 					<?php
 						}
 					?>
